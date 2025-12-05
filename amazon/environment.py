d8c205975ab27514c429bd47a4a8d6a2e34ae3a3
Ryan <d00416308@utahtech.edu>
2025-12-04 20:47:31 -0700

    trying to not be a bot

--- amazon/environment.py
+++ amazon/environment.py
@@ -7,6 +7,10 @@ def before_all(context):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
     context.behave_driver = behave_webdriver.Chrome(options=chrome_options)
 
 def after_all(context):
